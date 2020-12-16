49de9f74652d93eb9839481dcd46cb8d7869c367
jaume <jaume@westial.com>
2020-12-16 11:31:19 +0900

    Test cases described

---
+++ test/feature/turnoff.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const {describe, it, beforeEach} = require("mocha");
+const assert = require("assert");
+const sinon = require("sinon");
+
+describe("Turn off when the water reaches the maximum of the jar", function (){
+    it("does nothing if water is already off", function () {
+
+    });
+    it("watches water to do not pass over maximum level", function () {
+
+    });
+});
---
+++ test/feature/turnon.js
@@ -0,0 +1,10 @@
+'use strict';
+const {describe, it, beforeEach} = require("mocha");
+const assert = require("assert");
+const sinon = require("sinon");
+
+describe("Turn on the tap when I click on the button", function (){
+    it("makes water falling into the jar", function () {
+
+    });
+});
