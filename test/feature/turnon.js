2138572546a4f171e5d3d1e5be4b6b2405384ea8
jaume <jaume@westial.com>
2020-12-16 11:56:04 +0900

    Fake water and button

--- test/feature/turnon.js
+++ test/feature/turnon.js
@@ -5,6 +5,14 @@ const sinon = require("sinon");
 
 describe("Turn on the tap when I click on the button", function (){
     it("makes water falling into the jar", function () {
+        const button = sinon.fake();
+        button.onClick = sinon.fake();
+        button.click = sinon.fake();
+        const water = sinon.fake();
+        water.isFalling = sinon.stub(() => true);
+
+        button.onClick();
+        button.click();
         assert.strictEqual(water.isFalling(), true);
     });
 });
