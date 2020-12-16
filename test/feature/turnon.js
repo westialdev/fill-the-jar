48e3843892eb1d4faf43326ec2dc8120d6194ea9
jaume <jaume@westial.com>
2020-12-16 11:34:11 +0900

    Water is falling assertion

--- test/feature/turnon.js
+++ test/feature/turnon.js
@@ -5,6 +5,6 @@ const sinon = require("sinon");
 
 describe("Turn on the tap when I click on the button", function (){
     it("makes water falling into the jar", function () {
-
+        assert.strictEqual(water.isFalling(), true);
     });
 });
