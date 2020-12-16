f8a2266a6b464ba98525845d0e503550925d2079
jaume <jaume@westial.com>
2020-12-16 12:26:06 +0900

    Added no water falling when no click scenario

--- test/feature/turnon.js
+++ test/feature/turnon.js
@@ -6,16 +6,23 @@ const {Water} = require("../../src/core/water/water");
 const {Button} = require("../../src/core/button/button");
 
 describe("Turn on the tap when I click on the button", function (){
-    it("makes water falling into the jar", function () {
-        const water = Water();
-        water.fall = sinon.spy();
-        water.isFalling = sinon.stub(() => true);
+    const water = Water();
+    const button = Button();
 
-        const button = Button();
-
+    beforeEach(() => {
+        water.fall = sinon.spy(() => water.isFalling = sinon.stub(() => true));
+        water.isFalling = sinon.stub(() => false);
         button.onClick(water.fall);
+    });
+
+    it("makes water falling into the jar", function () {
         button.click();
         assert.strictEqual(water.isFalling(), true);
         assert.strictEqual(water.fall.calledOnce, true);
+    });
+
+    it("makes no falling water if button has not been clicked", function () {
+        assert.strictEqual(water.isFalling(), false);
+        assert.strictEqual(water.fall.calledOnce, false);
     });
 });
