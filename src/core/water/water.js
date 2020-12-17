559aa281a29b46a0d9930642e449f3dd688d0774
jaume <jaume@westial.com>
2020-12-17 14:05:36 +0900

    Turn off water if maximum level is got

--- src/core/water/water.js
+++ src/core/water/water.js
@@ -3,8 +3,12 @@
 exports.Water = function create() {
     const isFalling = () => {throw new Error("Not implemented")};
     const turnOn = () => {throw new Error("Not implemented")};
+    const turnOff = () => {throw new Error("Not implemented")};
+    const isMaxGot = () => {throw new Error("Not implemented")};
     return {
         turnOn,
-        isFalling
+        turnOff,
+        isFalling,
+        isMaxGot
     };
 };
--- test/feature/turnoff.js
+++ test/feature/turnoff.js
@@ -3,9 +3,24 @@
 const {describe, it, beforeEach} = require("mocha");
 const assert = require("assert");
 const sinon = require("sinon");
+const {Water} = require("../../src/core/water/water");
+
 
 describe("Turn off when the water reaches the maximum of the jar", function (){
-    it("stops water if maximum level is reached", function () {
+    const water = Water();
+
+    beforeEach(() => {
+        water.turnOn = sinon.spy(() => water.isFalling = sinon.stub(() => true));
+        water.turnOff = sinon.spy(() => water.isFalling = sinon.stub(() => false));
+        water.isFalling = sinon.stub(() => false);
+    });
+
+    it("cuts water if maximum level is got", function () {
+        water.isMaxGot = sinon.stub(() => true);
+        water.turnOn();
+
+        if (water.isMaxGot()) water.turnOff();
+        assert.strictEqual(water.isFalling(), false);
     });
     it("does nothing if water is already off", function () {
     });
